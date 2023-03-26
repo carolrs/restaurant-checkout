c82f454fb476718fc5aa6b1c92c7986b2116ee68
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-03-27 00:26:04 +0100

    adding more tests

--- spec/menu_integration_spec.rb
+++ spec/menu_integration_spec.rb
@@ -29,6 +29,25 @@ describe Menu do
 
     end
 
+    context"considering 2 differents menu" do
+      it "returns all availables dishes" do
+        dish_1 = Dish.new("Carbonara", 10.0)
+        dish_2 = Dish.new("Lasagna", 15.0)
+        dish_3 = Dish.new("Hamburguer", 9.0)
+        dish_4 = Dish.new("Pancake", 12.0)
+        dish_5 = Dish.new("Croissant", 3.0)
+        dish_6 = Dish.new("Scrabble eggs", 8.0)
+  
+        dish_availability = DishAvailability.new([dish_1, dish_2,dish_4, dish_5, dish_6])
+  
+        breakfast_menu = Menu.new([dish_4, dish_5, dish_6], dish_availability)
+        expect(breakfast_menu.all_available_dishes).to eq [dish_4, dish_5, dish_6]
+
+        dinner_menu = Menu.new([dish_1,dish_2,dish_3], dish_availability)
+        expect(dinner_menu.all_available_dishes).to eq [dish_1, dish_2]
+      end
+    end
+
     it "when no dishes available" do
       dish_1 = Dish.new("Carbonara", 10.0)
       dish_2 = Dish.new("Lasagna", 15.0)
@@ -38,7 +57,6 @@ describe Menu do
       menu = Menu.new([dish_1, dish_2], dish_availability)
 
       expect(menu.all_available_dishes).to eq  []
-
     end
   end
 end
